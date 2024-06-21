26d19221bf1a9c69a0f098b17440ae92d292f51b
diogo Gianezi de Melo <137850434+DiogoGMelo@users.noreply.github.com>
2024-06-21 17:39:18 -0300

    Update Patron.java

--- librarymanagement/src/main/java/grupo10/Patron.java
+++ librarymanagement/src/main/java/grupo10/Patron.java
@@ -1,15 +1,26 @@
+/**
+ * Represents a patron of the library.
+ * A patron has a name and an contact.
+ */
 public class Patron {
     private String name;
     private String contactInfo;
-
+    /**
+     * Constructs a new Patron with the specified name and ID.
+     */
     public Patron(String name, String contactInfo) {
         this.name = name;
         this.contactInfo = contactInfo;
     }
 
-    // Getters e setters
+    /**
+     * Gets the name of the patron.
+     */
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    /**
+     * Gets the contact of the patron.
+     */
     public String getContactInfo() { return contactInfo; }
     public void setContactInfo(String contactInfo) { this.contactInfo = contactInfo; }
 }
