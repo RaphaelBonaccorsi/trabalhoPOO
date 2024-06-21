e1320e12c5b5a149ba2cf382272324db7b2b6be8
diogo Gianezi de Melo <137850434+DiogoGMelo@users.noreply.github.com>
2024-06-21 17:10:40 -0300

    Update Book.java

--- librarymanagement/src/main/java/grupo10/Book.java
+++ librarymanagement/src/main/java/grupo10/Book.java
@@ -1,9 +1,17 @@
+/**
+ * Represents a book in the library system.
+ * A book has a title, author, ISBN, and availability status.
+ */
 public class Book {
     private String title;
     private String author;
     private String isbn;
     private String category;
 
+    /**
+     * Constructs a new Book with the specified title, author, and ISBN.
+     * The book is initially available.
+     */
     public Book(String title, String author, String isbn, String category) {
         this.title = title;
         this.author = author;
@@ -12,12 +20,27 @@ public class Book {
     }
 
     // Getters e setters
+    /**
+     * Gets the title of the book.
+     */
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
+     /**
+     * Gets the author of the book.
+     */
     public String getAuthor() { return author; }
     public void setAuthor(String author) { this.author = author; }
+
+    /**
+     * Gets the ISBN of the book.
+     */
     public String getIsbn() { return isbn; }
     public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    /**
+     * Gets the Category of the book.
+     */
     public String getCategory() { return category; }
     public void setCategory(String category) { this.category = category; }
 }
